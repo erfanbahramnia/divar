bbbe43f9de065feee0403345864e48df985aa9c7
erfanbahramnia <erfanbahramnia@gmail.com>
2024-02-24 23:13:38 +0330

    section of deleting account by user completed

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, InternalServerErrorException, Post, Request, UseGuards } from "@nestjs/common";
 import { UsersService } from "../service/users.service";
 import { UpdateUserDto } from "../dtos/updateUser.dto";
 import { AuthGuard } from "src/guards/auth.guard";
@@ -6,11 +6,11 @@ import { UserRequestData } from "src/interfaces/expresRequest.interface";
 import { ApiBearerAuth, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
 
 @ApiTags("Users")
+@UseGuards(AuthGuard)
 @Controller("/user")
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
-    @UseGuards(AuthGuard)
     @ApiOperation({summary: "user update it's data"})
     @ApiBearerAuth("JWT-AUTH")
     @ApiUnauthorizedResponse({description: "please login again"})
@@ -29,7 +29,29 @@ export class UsersController {
             };
         // success
         return {
+            status: HttpStatus.OK,
             result: "user data updated successfuly"
+        };
+    }
+
+    @ApiOperation({summary: "user delete it's account"})
+    @ApiBearerAuth("JWT-AUTH")
+    @ApiUnauthorizedResponse({description: "please login again"})
+    @ApiOkResponse({description: "data deleted successfuly"})
+    @ApiInternalServerErrorResponse({description: "Internal server error"})
+    @Get("/delete")
+    async DeleteUser(@Request() req: UserRequestData) {
+        // get user id
+        const { userId } = req.user;
+        // delete user
+        const result = await this.usersService.deleteUserById(userId);
+        // check user deleted or not
+        if(!result.affected) 
+            throw new InternalServerErrorException("server couldn't delete user!")
+        // success
+        return {
+            status: HttpStatus.OK,
+            message: "User deleted successfuly"
         };
     }
 }
